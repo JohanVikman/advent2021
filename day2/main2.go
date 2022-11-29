42799fd20c76e70840a7f1ccc83599bad3be552a
Johan Vikman Lundström (jvikman) <jvikman@cisco.com>
2022-11-29 14:48:43 +0100

    Day2 part 2

---
+++ day2/main2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	fileScanner := bufio.NewScanner(f)
+
+	fileScanner.Split(bufio.ScanLines)
+	horiz := 0
+	depth := 0
+	aim := 0
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		fmt.Printf("%v\n", line)
+		parts := strings.Split(line, " ")
+		direction := parts[0]
+		moveAmount, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch direction {
+		case "forward":
+			{
+				horiz += moveAmount
+				depth += aim * moveAmount
+			}
+		case "up":
+			{
+				aim -= moveAmount
+			}
+		case "down":
+			{
+				aim += moveAmount
+			}
+		}
+	}
+	fmt.Printf("%v, %v = %v", horiz, depth, horiz*depth)
+}
